ec686df3820e7ddd341a3494a7203ae38584a2c4
ChalitaK24 <schalitakel@gmail.com>
2024-10-29 15:27:47 -0700

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,19 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Bubble sort implemetation funcion
+ * @new:
+ *
+ *
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int a;
+	int swap;
+
+	if(array == NULL)
+		return (0);
+
+
+}
