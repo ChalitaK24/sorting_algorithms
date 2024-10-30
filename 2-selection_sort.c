198bff8f301af9f3a4233fcba79bdb4e4f8388d4
ChalitaK24 <schalitakel@gmail.com>
2024-10-30 14:41:12 -0700

    sorting selection

---
+++ 2-selection_sort.c
@@ -0,0 +1,20 @@
+#include "sort.h"
+
+/**
+ * selection_sort - function that sorts an array of int
+ * ascending order using the Selection sort algorithm
+ *
+ *
+ * Return: void
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	if (!array)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+
+
+
+}
