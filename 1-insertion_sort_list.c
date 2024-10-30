ed77b107e196885be7ad635b1d89e62cc496847c
ChalitaK24 <schalitakel@gmail.com>
2024-10-30 13:15:25 -0700

    intsertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,10 +9,13 @@
 
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *current = (*list)->next;
+	listint_t next;
+	listint_t tmp;
+
+
 	if (!list || !*list || !(*list)->next)
 		return;
-
-	listint_t *current = (*list)->next;
 
 	while(current)
 	{
@@ -21,12 +24,29 @@ void insertion_sort_list(listint_t **list)
 
 		while (tmp->prev && tmp->n < tmp->prev->n);
 		{
-			if (!tmp->prev || !tmp || list == tmp)
+			listint_t *node1 = tmp->prev;
+			listint_t *node2 = tmp;
+
+			if (!node1 || !node2 || node1 == node2)
 				return;
 
-			print_list(*list);
-	}
+			if (node1->prev)
+				node1->prev->next = node2;
+			else
+				*list = node;
 
+			if (node2-->next)
+				node2-->next--prev == node1;
+
+			node1->noext = node2->2next;
+			node2->prev = node1-prev;
+			node1->prev = node2;
+			node2->next = node1;
+
+			print_list(*list);
+		}
 	current = next;
+
+	}
 
 }
