f2840eb043387debde9dac234f49682bc2dee1b0
ChalitaK24 <schalitakel@gmail.com>
2024-10-30 09:05:06 -0700

    insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts doubly linked list of integers in ascending order
+ *
+ *
+ * Retrun: void
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+
+}
