d6653aa393e25012eb19674349d30be3cda4c2eb
ChalitaK24 <schalitakel@gmail.com>
2024-10-30 12:49:59 -0700

    inser sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,5 +9,24 @@
 
 void insertion_sort_list(listint_t **list)
 {
+	if (!list || !*list || !(*list)->next)
+		return;
+
+	listint_t *current = (*list)->next;
+
+	while(current)
+	{
+		*next = current->next;
+		*tmp = current;
+
+		while (tmp->prev && tmp->n < tmp->prev->n);
+		{
+			if (!tmp->prev || !tmp || list == tmp)
+				return;
+
+			print_list(*list);
+	}
+
+	current = next;
 
 }
