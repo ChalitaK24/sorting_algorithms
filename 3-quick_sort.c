a79ee7bd12059fc9a988bc7475838ca34a276d3c
ChalitaK24 <schalitakel@gmail.com>
2024-10-30 16:29:52 -0700

    quuick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * quick_sort - function sorts an array of integers in
+ * ascending order using the quick sort algorithm
+ *
+ *
+ * Return: void
+ */
+
+void quick_sort(int *array, size_t size)
