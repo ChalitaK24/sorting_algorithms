5f4024963c6b52671b66ff3c5c5375a390e85e5f
ChalitaK24 <schalitakel@gmail.com>
2024-10-30 07:39:28 -0700

    sort bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,11 +9,26 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	int a;
-	int swap;
+	int start;
+	int swap = 0;
+	size_t i = 0;
 
 	if(array == NULL)
 		return (0);
 
+	for(size_t i; i < size - 1; i++)
+	{
+		for (size_t j; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j +1])
+			{
+				start = array[j]
+				array[j] = array[j+1 1];
+				array[j + 1] = start;
 
+				print_array(array, size);
+				swap = 1;
+			}
+		}
+	}
 }
